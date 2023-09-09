4a60c6ef260149a7d84ab596fc1ac21fac58858c
Khancat1120 <1755675467@qq.com>
2023-09-09 17:31:10 +0800

    code

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,14 +81,14 @@ module.exports = {
         // 项目开始时间
         startYear: '2020',
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/Khancat1120/Khancat1120.github.io/',
+        repo: 'https://github.com/Khancat1120/vuepress-blog',
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: 'Github',
         // 以下为可选的编辑链接选项
         // 假如你的文档仓库和项目本身不在一个仓库：
-        docsRepo: 'Khancat1120/vuepress-blog',
-        editLinkPattern: 'https://github.com/Khancat1120/vuepress-blog/edit/main/docs/',
+        // docsRepo: 'Khancat1120/vuepress-blog',
+        // editLinkPattern: 'https://github.com/Khancat1120/vuepress-blog/edit/main/docs/',
         // 假如文档不是放在仓库的根目录下：
         // docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
