6659c4a9fa124cc470271c9e99a1f6223aba0ab3
Khancat1120 <1755675467@qq.com>
2023-09-09 17:38:13 +0800

    code

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -88,7 +88,7 @@ module.exports = {
         // 以下为可选的编辑链接选项
         // 假如你的文档仓库和项目本身不在一个仓库：
         // docsRepo: 'Khancat1120/vuepress-blog',
-        // editLinkPattern: 'https://github.com/Khancat1120/vuepress-blog/edit/main/docs/',
+        editLinkPattern: 'https://github.com/Khancat1120/vuepress-blog/edit/main/docs/',
         // 假如文档不是放在仓库的根目录下：
         docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
