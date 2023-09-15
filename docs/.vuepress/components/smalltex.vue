d36b2566b15cf4adaf5c4f8e2d4530c332fd5ecc
Khancat1120 <1755675467@qq.com>
2023-09-15 18:39:26 +0800

    code

--- docs/.vuepress/components/smalltex.vue
+++ docs/.vuepress/components/smalltex.vue
@@ -1,5 +1,5 @@
 <template>
-  <span ref="latex" class="katex" v-html="rawHTML" style="font-size: 70%;"></span>
+  <span ref="latex" class="katex" v-html="rawHTML" style="font-size: 100%;"></span>
 </template>
 
 <script>
